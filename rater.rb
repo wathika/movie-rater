583dc3126b5e422e04d6c1cf569f6e7327664d23
wathika <vcwathika@gmail.com>
2015-08-23 20:03:43 +0300

    added prompted choices

--- rater.rb
+++ rater.rb
@@ -1,7 +1,12 @@
 movies = {
     "iron man" => 5,
     }
-puts "what do you wanna do?"
+    puts "What would you like to do?"
+    puts "-- Type 'add' to add a movie."
+    puts "-- Type 'update' to update a movie."
+    puts "-- Type 'display' to display all movies."
+    puts "-- Type 'delete' to delete a movie."
+
 choice = gets.chomp
 
 case choice
