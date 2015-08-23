9f8a72b10d4968734999c0add353e0a68914fda6
wathika <vcwathika@gmail.com>
2015-08-23 19:41:51 +0300

    done with add case

--- rater.rb
+++ rater.rb
@@ -8,9 +8,13 @@ case choice
 when "add"
   puts "What is the name of your movie??"
   title = gets.chomp
-  puts "How do you want to rate the movie #{title}?"
-  rating = gets.chomp
-  movies[title.to_sym] = rating.to_i
+  if movies[title.to_sym].nil?
+    puts "How do you want to rate the movie #{title}?"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  else
+    puts "Sorry that movie already exists!!"
+  end
 when "update"
     puts "Updated!"
 when "display"
