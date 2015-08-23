923fbb09e6135a6d7952a8594fc5978535883236
wathika <vcwathika@gmail.com>
2015-08-23 15:30:44 +0300

    gives a user choices to choose from

--- rater.rb
+++ rater.rb
@@ -1,5 +1,18 @@
 movies = {
     "iron man" => 5,
     }
-puts "whats your movie?"
+puts "what do you wanna do?"
 choice = gets.chomp
+
+case choice
+when "add"
+    puts "Added!"
+when "update"
+    puts "Updated!"
+when "display"
+    puts "Movies!"
+when "delete"
+    puts "Deleted!"
+else
+    puts "Error!"
+end
